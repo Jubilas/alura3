7955513775877746076fae380c83e608b25698bf
Jubilas <cauaferreira2676@gmail.com>
2026-08-18 03:09:04 -0300

    So aplica o substituto do sqlite3 se ele não for suficiente, antes ele forçava mesmo quando tinha e falhava silenciosamente.

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ Interface interativa com chat, gestão de documentos (PDF/CSV) e visualização 
 Design sóbrio, minimalista e corporativo sem emojis.
 Todas as funções, comentários e textos em Português (PT-BR).
 """
-
-__import__('pysqlite3')
-import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import sqlite3
+if sqlite3.sqlite_version_info < (3, 35, 0):
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import os
 import time
 from datetime import datetime
